b1d383152d10ee8f2aef5852830d99773efe4125
Olga Prohorova <prohorova-os@yandex.ru>
2021-03-19 13:11:47 +0500

    fix: исправление ошибок после ревью2

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,9 @@ let closeOverlay = document.querySelector('.popup__overlay');
 let popupContent = document.querySelector('.popup__content');
 let editTitle = document.querySelector('.profile__title');
 let editSubtitle = document.querySelector('.profile__subtitle');
-let formElement = document.forms.popup_form;
-let editT = formElement.elements.edit_title;
-let editS = formElement.elements.edit_subtitle;
-
-editT.value = editTitle.textContent;
-
-console.log(editT.textContent);
+let formElement = document.querySelector('.form');
+let editT = formElement.elements.edittitle;
+let editS = formElement.elements.editsubtitle;
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
