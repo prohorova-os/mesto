dcdee8bebb8b11523ae878dd5d0d689d663d678d
Olga Prohorova <prohorova-os@yandex.ru>
2021-04-02 09:30:10 +0500

    fix: исправить замечания после ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -26,118 +26,51 @@ const intitalListData = [
   ]; 
 
 /*popap редактировать: переменные*/
-let popup = document.querySelector('.popup');
-let openPopupBtn = document.getElementById('open_popup_btn');
-let closePopupBtn = document.querySelector('.popup__close-btn');
-let closeOverlay = document.querySelector('.popup__overlay');
-let popupContent = document.querySelector('.popup__content');
-let editTitle = document.querySelector('.profile__title');
-let editSubtitle = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('[name="popup-form"]');
-let editT = document.querySelector('[name="edit-title"]');
-let editS = document.querySelector('[name="edit-subtitle"]');
+const popup = document.querySelector('.popup');
+const openPopupBtn = document.getElementById('open_popup_btn');
+const closePopupBtn = document.querySelector('.popup__close-btn');
+const closeOverlay = document.querySelector('.popup__overlay');
+const popupContent = document.querySelector('.popup__content');
+const editTitle = document.querySelector('.profile__title');
+const editSubtitle = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('[name="popup-form"]');
+const editT = document.querySelector('[name="edit-title"]');
+const editS = document.querySelector('[name="edit-subtitle"]');
 
 /*popap новое место: переменные*/
-let openPopupAddBtn = document.getElementById('open_popup-add_btn');
-let closePopupAddBtn = document.getElementById('close_popup-add_btn');
-let closePopupAddOverlay = document.querySelector('[name="add-popup-overlay"]');
-let popupAdd = document.getElementById('add-popup');
-let profileAddBtn = document.querySelector('.profile__add-btn');
-let formElementAdd = document.querySelector('[name="add-popup-form"]');
-let editTAdd = document.querySelector('[name="edit-title-add"]');
-let editSAdd = document.querySelector('[name="edit-subtitle-add"]');
+const openPopupAddBtn = document.getElementById('open_popup-add_btn');
+const closePopupAddBtn = document.getElementById('close_popup-add_btn');
+const closePopupAddOverlay = document.querySelector('[name="add-popup-overlay"]');
+const popupAdd = document.getElementById('add-popup');
+const profileAddBtn = document.querySelector('.profile__add-btn');
+const formElementAdd = document.querySelector('[name="add-popup-form"]');
+const editTAdd = document.querySelector('[name="edit-title-add"]');
+const editSAdd = document.querySelector('[name="edit-subtitle-add"]');
 
 /*popup просмотр картинки: переменные*/
 const popupImg = document.getElementById('img-popup');
+const closePopupImgBtn = document.getElementById('close_popup-img_btn');
+const closePopupImgOverlay = document.querySelector('[name="img-popup-overlay"]');
 
 /*вставка картинок на сайт: переменные*/
 const list = document.querySelector('.elements');
 const listItemTemplate = document.querySelector('.list-item-template').content.querySelector('.element');
 
-/*popap редактировать: функции*/
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    editTitle.textContent = editT.value;
-    editSubtitle.textContent = editS.value;
-    closePopup();                
-}
-    
-function openPopup(event) {;
+/*popap для всех: функции*/
+function openPopup(popup) {;
     popup.classList.remove('popup_closed');
     popup.classList.add('popup_opened');
-    editT.value = editTitle.textContent;
-    editS.value = editSubtitle.textContent;
 }
 
-function closePopup() {
+function closePopup(popup) {
     popup.classList.remove('popup_opened');
     popup.classList.add('popup_closed');
 }
 
-/*popap новое место: функции*/
-function openPopupAdd(event) {
-    popupAdd.classList.remove('popup_closed');
-    popupAdd.classList.add('popup_opened');
-}
-
-function closePopupAdd() {
-    popupAdd.classList.remove('popup_opened');
-    popupAdd.classList.add('popup_closed');
-    editTAdd.value = '';
-    editSAdd.value='';
-}
-
-function openPopupImg(event) {
-    popupImg.classList.remove('popup_closed');
-    popupImg.classList.add('popup_opened');
-}
-
-function closePopupImg() {
-    popupImg.classList.remove('popup_opened');
-    popupImg.classList.add('popup_closed');
-}
-
-/* popap новое место: функции: добавление элемента на сайт*/
-function formAddSubmitHandler (evt) {
-    evt.preventDefault();
-    const listItem = listItemTemplate.cloneNode(true);
-
-    const link = editSAdd.value;
-    const name = editTAdd.value;
-    /*создание элемента*/
-    createListItem(listItem, link, name);
-    /*удаление элемента*/
-    deleteListItem(listItem);
-    /*popup просмотр картинки*/
-    viewingListItem(listItem, link, name);
-
-    list.prepend(listItem);
-    
-    closePopupAdd();          
-}
-
-/*popup просмотр картинки: функции*/
-function viewingListItem(item, link, name) {
-    const listItemImage = item.querySelector('.element__foto');
-    /*обработка событий*/
-    listItemImage.addEventListener('click', () => {
-        openPopupImg();
-        const popupImgSrc = popupImg.querySelector('.popup__img').src = link;
-        const popupImgAlt = popupImg.querySelector('.popup__img').alt = name;
-        const popupImgTitle = popupImg.querySelector('.popup__title-img').textContent = name;
-    });
-
-    const closePopupImgBtn = document.getElementById('close_popup-img_btn');
-    const closePopupImgOverlay = document.querySelector('[name="img-popup-overlay"]');
-    /*обработка событий*/
-    closePopupImgBtn.addEventListener('click', function() {closePopupImg();});
-    closePopupImgOverlay.addEventListener('click', function() {closePopupImg();});
-}
-
 /*вставка картинок на сайт: функции: создание элемента*/
 function createListItem(item, link, name) {
-    const listItemImageSrc = item.querySelector('.element__foto').src = link;
-    const listItemImageAlt = item.querySelector('.element__foto').alt = name;
+    item.querySelector('.element__foto').src = link;
+    item.querySelector('.element__foto').alt = name;
 
     const listItemLike = item.querySelector('.element__like');
     listItemLike.addEventListener('click', () => {
@@ -146,6 +79,7 @@ function createListItem(item, link, name) {
     const listItemTitle = item.querySelector('.element__title');
     listItemTitle.textContent = name;
 }
+
 /*вставка картинок на сайт: функции: удаление элемента*/
 function deleteListItem(item) {
     const deleteBtn = item.querySelector('.element__delete');
@@ -154,31 +88,77 @@ function deleteListItem(item) {
     });
 }
 
-/*popap редактировать: обработка событий*/
-openPopupBtn.addEventListener('click', function() {openPopup();});
-closePopupBtn.addEventListener('click', function() {closePopup();});
-closeOverlay.addEventListener('click', function() {closePopup();});
-formElement.addEventListener('submit', formSubmitHandler);
-
-/*popap новое место: обработка событий*/
-openPopupAddBtn.addEventListener('click', function() {openPopupAdd();});
-closePopupAddBtn.addEventListener('click', function() {closePopupAdd();});
-closePopupAddOverlay.addEventListener('click', function() {closePopupAdd();});
-formElementAdd.addEventListener('submit', formAddSubmitHandler);
-
-
-/*вставка картинок на сайт*/
-intitalListData.forEach(item => {
+/*создание и удаление элемента, popap просмотр картинки*/
+function actionsListItem(listItemTemplate, link, name) {
     const listItem = listItemTemplate.cloneNode(true);
-
-    const link = item.link;
-    const name = item.name;
     /*создание элемента*/
     createListItem(listItem, link, name);
     /*удаление элемента*/
     deleteListItem(listItem);
     /*popup просмотр картинки*/
     viewingListItem(listItem, link, name);
-
     list.prepend(listItem);
+    return listItem;
+}
+
+/*popap редактировать: функции*/
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    editTitle.textContent = editT.value;
+    editSubtitle.textContent = editS.value;
+    closePopup(popup);                
+}
+
+/* popap новое место: функции: добавление элемента на сайт*/
+function formAddSubmitHandler (evt) {
+    evt.preventDefault();
+    const link = editSAdd.value;
+    const name = editTAdd.value;
+    /*создание и удаление элемента, popap просмотр картинки*/
+    actionsListItem(listItemTemplate, link, name);
+    closePopup(popupAdd);          
+}
+
+/*popup просмотр картинки: функции*/
+function viewingListItem(item, link, name) {
+    const listItemImage = item.querySelector('.element__foto');
+    /*обработка событий*/
+    listItemImage.addEventListener('click', () => {
+        openPopup(popupImg);
+        popupImg.querySelector('.popup__img').src = link;
+        popupImg.querySelector('.popup__img').alt = name;
+        popupImg.querySelector('.popup__title-img').textContent = name;
+    });
+}
+
+/*popap редактировать: обработка событий*/
+openPopupBtn.addEventListener('click', function() {
+        openPopup(popup);
+        editT.value = editTitle.textContent;
+        editS.value = editSubtitle.textContent;
+    });
+closePopupBtn.addEventListener('click', function() {closePopup(popup);});
+closeOverlay.addEventListener('click', function() {closePopup(popup);});
+formElement.addEventListener('submit', formSubmitHandler);
+
+/*popap новое место: обработка событий*/
+openPopupAddBtn.addEventListener('click', function() {
+        openPopup(popupAdd);
+        editTAdd.value = '';
+        editSAdd.value='';
+    });
+closePopupAddBtn.addEventListener('click', function() {closePopup(popupAdd);});
+closePopupAddOverlay.addEventListener('click', function() {closePopup(popupAdd);});
+formElementAdd.addEventListener('submit', formAddSubmitHandler);
+
+/*popup просмотр картинки: обработка событий*/
+closePopupImgBtn.addEventListener('click', function() {closePopup(popupImg);});
+closePopupImgOverlay.addEventListener('click', function() {closePopup(popupImg);});
+
+/*вставка картинок на сайт*/
+intitalListData.forEach(item => {
+    const link = item.link;
+    const name = item.name;
+    /*создание и удаление элемента, popap просмотр картинки*/
+    actionsListItem(listItemTemplate, link, name);
 });
